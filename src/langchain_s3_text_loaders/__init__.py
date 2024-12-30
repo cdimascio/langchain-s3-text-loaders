dd3cf4832c5e1d4e88543ba37420c99c4ae41006
carmine <carmine@everco.ai>
2024-12-30 11:55:34 -0500

    restructure document_loaders

--- src/langchain_s3_text_loaders/__init__.py
+++ src/langchain_s3_text_loaders/__init__.py
@@ -1,2 +1,3 @@
-from .s3_text_directory_loader import S3TextFileDirectoryLoader
-from .s3_text_file_loader import S3TextFileLoader
+from .document_loaders import S3TextFileDirectoryLoader, S3TextFileLoader
+
+__all__ = ["S3TextFileDirectoryLoader", "S3TextFileLoader"]
